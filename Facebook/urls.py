47495848f2426c68f454c5a1a26bfd7bd003243a
Imcber <imcber1@gmail.com>
2016-08-19 01:17:53 -0500

    ya se pueden hascer post, mientras estes loggeado

--- Facebook/urls.py
+++ Facebook/urls.py
@@ -6,8 +6,9 @@ from accounts import urls as cuentasUrls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'^posts/$', views.UpdateView.as_view(), name="nuevo"),
-     #url(r'^blog/(?P<slug>[-\w]+)/$', views.DetailView.as_view(), name="detalle"),
+     url(r'^posts/new/$', views.UpdateView.as_view(), name="new"),
+     url(r'^posts/$', views.ListView.as_view(), name = "losPosts"),
+     url(r'^posts/(?P<slug>[-\w]+)/$', views.DetailView.as_view(), name="detalle"),
      url(r'^',include(cuentasUrls))
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
+from .models import Post
+from .forms import PostForm
+
+from django.utils.text import slugify
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+class ListView(View):
+	def get(self,request):
+		template_name="losPosts.html"
+		posts = Post.objects.all().order_by('fecha')
+		context = {
+		'posts':posts
+		}
+		return render(request,template_name,context)
+
+class DetailView(View):
+	def get(self,request,slug):
+		template_name="detalle.html"
+		post = Post.objects.get(slug = slug)
+		context = {
+		'post':post
+		}
+		return render(request,template_name,context)
 
 class UpdateView(View):
+	@method_decorator(login_required)
 	def get(self,request):
 		template_name = 'new.html'
-		context = {}
+		form = PostForm()
+		context = {
+		'form':form
+		}
 		return render(request, template_name, context)
+
+	def post(self,request):
+		form = PostForm(request.POST)
+		new_post = form.save(commit = False)
+		new_post.slug = slugify(new_post.titulo)
+		new_post.save()
+		return redirect('losPosts')
